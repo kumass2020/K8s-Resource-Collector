500cdbec62ea64c5525a2d62c308d74395aed4c5
hoho <kumass2021@gmail.com>
2022-10-06 15:03:49 +0900

    fix: namespace str handling

--- resource_collector.py
+++ resource_collector.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 from os import path
-
 output = subprocess.check_output('kubectl get namespace --all-namespaces -o=name', shell=True)
 output_list = str(output).lstrip("b'").rstrip("'").split('\\n')
 output_list.pop()
@@ -12,12 +11,13 @@ current_path = os.getcwd()
 
 namespace_list = []
 for i, element in enumerate(output_list):
-    element.lstrip('namespace/')
+    element = element.lstrip('namespace/')
     namespace_list.append(element)
 
 resource_type_str = 'pvc,configmap,serviceaccount,secret,service,deployment,statefulset,hpa,job,cronjob,node,namespace,endpoints,pv,rolebindings,roles,netpol,replicasets,pdb,pod'
 resource_type_list = resource_type_str.split(',')
 for namespace in namespace_list:
+    print('init ' + namespace + '...')
     for resource_type in resource_type_list:
         resource_list = []
         output = subprocess.check_output('kubectl get -n ' + namespace + ' -o=name ' + resource_type, shell=True)
@@ -29,7 +29,14 @@ for namespace in namespace_list:
             resource_list.append(element)
             for resource in resource_list:
                 subprocess.check_output('mkdir -p $(dirname ' + resource + ')', shell=True)
-                output = subprocess.check_output('kubectl get -n ' + namespace + ' -o=yaml ' + resource + ' > ' + resource + '.yaml', shell=True)
+                while True:
+                    output = subprocess.check_output('kubectl get -n ' + namespace + ' -o=yaml ' + resource + ' > ' + resource + '.yaml', shell=True)
+                    if 'refused' in str(output):
+                        print('refused')
+                        pass
+                    else:
+                        print('got yaml')
+                        break
 
 subprocess.check_output('cd ..', shell=True)
 subprocess.check_output('python3 /home/ccl/filled_file_collector.py', shell=True)
